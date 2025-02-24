2cf8bada2208eb9948e10cc59a0c42a5ebe439a2
Edoardo Nicoli Castiglia <edoardonica03@gmail.com>
2025-02-25 00:48:32 +0100

    Aggiunto Cell.h

---
+++ Cell.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 25/02/2025.
+//
+
+#include "Cell.h"
---
+++ Cell.h
@@ -0,0 +1,27 @@
+//
+// Created by Asus on 25/02/2025.
+//
+
+#ifndef LABORATORIO_CELL_H
+#define LABORATORIO_CELL_H
+#include "Observer.h"
+#include <vector>
+
+//classe Cell, che rappresenta una cella della griglia del foglio di calcolo
+class Cell {
+public :
+Cell (double value = 0); //costruttore
+ void setValue (double newValue); //metodo per settare il valore della cella
+    double getValue () const; //metodo per ottenere il valore della cella
+    void subscribe (Observer* o ) ; //metodo per aggiungere un observer alla lista di observer
+    void unsubscribe (Observer* o ) ; //metodo per rimuovere un observer dalla lista di observer
+    void notify () ; //metodo per notificare tutti gli observer
+
+private :
+    double value ; //valore della cella
+    std :: vector <Observer*> observers ; //lista di observer
+
+};
+
+
+#endif //LABORATORIO_CELL_H
