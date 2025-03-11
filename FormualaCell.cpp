3ab630409ca9db9ebcd8c4a1f1ce55d41dc0bbe6
Edoardo Nicoli Castiglia <edoardonica03@gmail.com>
2025-03-11 23:10:31 +0100

    Aggiunta classe FormualaCell.h

---
+++ FormualaCell.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 26/02/2025.
+//
+
+#include "FormualaCell.h"
---
+++ FormualaCell.h
@@ -0,0 +1,28 @@
+//
+// Created by Asus on 26/02/2025.
+//
+
+#ifndef LABORATORIO_FORMUALACELL_H
+#define LABORATORIO_FORMUALACELL_H
+#include "Observer.h"
+#include "Cell.h"
+#include <vector>
+
+enum FormulaType { SUM, MAX, MIN, AVG }; //enumerazione per i tipi di formule possibili (somma, massimo, minimo, media)
+
+class FormulaCell : public Observer {
+public :
+    FormulaCell (const std:: vector<Cell*>& cells, FormulaType type); //costruttore
+    void update() override; //metodo per aggiornare il valore della cella
+    double getValue() const; //metodo per ottenere il valore della cella
+private:
+    std :: vector<Cell*> cells; //lista di celle
+    FormulaType type; //tipo di formula
+    double value; //valore della cella
+    void calculateValue(); //metodo per calcolare il valore della cella
+
+
+};
+
+
+#endif //LABORATORIO_FORMUALACELL_H
