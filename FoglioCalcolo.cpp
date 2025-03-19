42534353535968328ba9426b8422b69d4bfec4da
Edoardo Nicoli Castiglia <edoardonica03@gmail.com>
2025-03-19 13:11:59 +0100

    Aggiunta classe Foglio di Calcolo

---
+++ FoglioCalcolo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 19/03/2025.
+//
+
+#include "FoglioCalcolo.h"
---
+++ FoglioCalcolo.h
@@ -0,0 +1,19 @@
+//
+// Created by Asus on 19/03/2025.
+//
+
+#ifndef LABORATORIO_FOGLIOCALCOLO_H
+#define LABORATORIO_FOGLIOCALCOLO_H
+#include <vector>
+#include "Cell.h"
+#include "FormualaCell.h"
+
+
+class FoglioCalcolo {
+private:
+    std::vector<Cell*> cells; //lista di celle
+
+};
+
+
+#endif //LABORATORIO_FOGLIOCALCOLO_H
